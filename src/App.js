93c2dc645f47718977342f3d5d715985f164b611
Nate <nate@roon.care>
2025-12-12 15:39:10 -0500

    Fix React Router basename and GitHub Pages 404s

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/puzzle-collection">
       <div className="App">
         <Header />
         <main className="main-content">
